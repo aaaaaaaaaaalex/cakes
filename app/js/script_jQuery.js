3d48faf2b95cc9746694e9740ec9e905d2776201
aaaaaaaaaaalex <aleks.ru.mail.77@mail.ru>
2021-05-23 12:54:16 +0400

    начал работу над блоком job

--- app/js/script_jQuery.js
+++ app/js/script_jQuery.js
@@ -8,4 +8,15 @@ document.addEventListener("DOMContentLoaded", function(){
 		blockBg.classList.add("scale-intro");
 	};
 	setTimeout(addClass, 50);
+
+
+	// Canvas
+	let canvas = document.getElementById("canvas");
+	let ctx = canvas.getContext("2d");
+
+	ctx.setLineDash([5, 15]);/*dashes are 5px and spaces are 15px*/
+	ctx.beginPath();
+	ctx.moveTo(0,100);
+	ctx.lineTo(400, 100);
+	ctx.stroke();
 });
