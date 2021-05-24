ac86ce70468b491a8066d4df9c061061243005ae
aaaaaaaaaaalex <aleks.ru.mail.77@mail.ru>
2021-05-24 09:40:30 +0400

    начал работу с svg

--- app/js/script_jQuery.js
+++ app/js/script_jQuery.js
@@ -10,20 +10,45 @@ document.addEventListener("DOMContentLoaded", function(){
 	setTimeout(addClass, 50);
 
 
-	// Canvas
-	let canvas = document.getElementById("canvas");
-	let ctx = canvas.getContext("2d");
+	// Svg
+	let path = document.querySelector(".svg__path");
+	let numPoints = 4;
+	let points = [100, 200, 800, 100];
+	let str = "";
+	// str += `M 0 0 V ${points[0]}`;
+	str += `M 0 ${points[0]}`;
 
-	canvas.setAttribute("width", "1170px");
-	canvas.setAttribute("height", "400px");
+
+	for (let i=0; i < numPoints-1; i++) {
+		let p = i+1/(numPoints-1)*100;
+		let cp = p - (1/(numPoints-1)*100)/2;
+		str += `C ${cp} ${points[i]} ${cp} ${points[i+1]} ${p} ${points[i+1]}`;
+	};
+	// str += "V 0 H 0"
+
+	path.setAttribute("d", str);
+
+
+
+
+
+
+
+
+	// Canvas
+	// let canvas = document.getElementById("canvas");
+	// let ctx = canvas.getContext("2d");
+
+	// canvas.setAttribute("width", "1170px");
+	// canvas.setAttribute("height", "400px");
 	
-	ctx.strokeStyle = '#f00';
-	ctx.beginPath();
-	ctx.moveTo(10, 15);
-	ctx.bezierCurveTo(75, 55, 175, 20, 250, 15);
-	ctx.moveTo(10, 15);
-	ctx.quadraticCurveTo(100, 100, 250, 15);
-	ctx.stroke();
+	// ctx.strokeStyle = '#f00';
+	// ctx.beginPath();
+	// ctx.moveTo(10, 15);
+	// ctx.bezierCurveTo(75, 55, 175, 20, 250, 15);
+	// ctx.moveTo(10, 15);
+	// ctx.quadraticCurveTo(100, 100, 250, 15);
+	// ctx.stroke();
 
 	// canvas.height = 
 	// ctx.setLineDash([5, 15]);/*dashes are 5px and spaces are 15px*/
